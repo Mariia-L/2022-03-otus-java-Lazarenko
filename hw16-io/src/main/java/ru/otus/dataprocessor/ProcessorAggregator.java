4b1a73c015a175992f3b05b56fba1a009cea6d70
Mariia L <mariya.v.lazarenko@gmail.com>
2022-06-22 18:15:39 +0300

    fix

--- hw16-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -2,10 +2,7 @@ package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
 
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
@@ -14,9 +11,6 @@ public class ProcessorAggregator implements Processor {
     public Map<String, Double> process(List<Measurement> data) {
         //группирует выходящий список по name, при этом суммирует поля value
         return data.stream()
-                .collect(Collectors.toMap(Measurement::getName, Measurement::getValue, Double::sum))
-                .entrySet().stream()
-                .sorted(Comparator.comparingDouble(Map.Entry::getValue))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,(e1, e2) -> e1, LinkedHashMap::new));
+                .collect(Collectors.toMap(Measurement::getName, Measurement::getValue, Double::sum, TreeMap::new));
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,13 +1,12 @@
 package ru.otus.dataprocessor;
 
-import com.google.common.io.Resources;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
@@ -21,7 +20,12 @@ public class ResourcesFileLoader implements Loader {
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
-        try (FileReader fileReader = new FileReader(Resources.getResource(fileName).getPath())) {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
+
+        if (inputStream == null)
+            return Collections.emptyList();
+
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(inputStream))) {
             Gson gson = new Gson();
             URL url = getClass().getClassLoader().getResource(fileName);
 
